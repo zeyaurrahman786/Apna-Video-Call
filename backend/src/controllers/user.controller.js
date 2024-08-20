4d49e4624fdf9e4f99587b9e0b0e1bf8f9f507f5
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-08-20 13:49:18 +0530

    Backend

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,50 @@
+import httpStatus from "http-status";
+import { User } from "../models/user.model";
+import bcrypt, { hash } from "bcrypt";
+
+
+const register = async (req, res) => {
+  const { name, username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(httpStatus.FOUND).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: name,
+      username: username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(httpStatus.CREATED).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.json({ message: `Something went wrong ${err}` });
+  }
+};
+
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ message: "Please Provide" });
+    }
+    try {
+        const user = await User.find({ username });
+        if (!user) {
+            return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" });
+        }
+        if (bcrypt.compare(user.password, password)) {
+            let token = crypto.randomBytes(20).toString("hex");
+            user.token = token;
+            await user.save();
+            return res.status(httpStatus.OK).json({ token: token });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: `Something went wrong ${err}` });
+    }
+};
+
+
+export { register, login };
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -3,8 +3,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.route("/login")
-router.route("/register")
+router.route("/login").post(login)
+router.route("/register").post(register)
 router.route("/add_to_activity")
 router.route("/get_all_activity")
 
