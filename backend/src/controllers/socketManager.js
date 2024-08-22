28d31243c218491c92f7812d227be36f63a7698a
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-08-22 12:35:14 +0530

    socketManager

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -5,7 +5,14 @@ let messages = {};
 let timeOnline = {};
 
 export const connectToSocket = (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["*"],
+      credentials: true,
+    },
+  });
 
   io.on("connection", (socket) => {
 
