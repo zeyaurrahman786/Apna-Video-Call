b5307e588739ac0a6b40b0cf2a86b60003ed736e
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-08-25 11:36:13 +0530

    Zoom

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -453,11 +453,9 @@ export default function VideoMeetComponent() {
 
                 <div>
 
-
                     <h2>Enter into Lobby </h2>
                     <TextField id="outlined-basic" label="Username" value={username} onChange={e => setUsername(e.target.value)} variant="outlined" />
                     <Button variant="contained" onClick={connect}>Connect</Button>
-
 
                     <div>
                         <video ref={localVideoref} autoPlay muted></video>
