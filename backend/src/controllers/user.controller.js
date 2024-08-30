ca65861b252deec6a4d5cd30e4f10149ac727010
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-08-30 22:33:41 +0530

    Zoom

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,7 +2,43 @@ import httpStatus from "http-status";
 import { User } from "../models/user.model.js";
 import bcrypt, { hash } from "bcrypt";
 import crypto from "crypto";
+import { Meeting } from "../models/meeting.model.js";
 
+
+
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please Provide" });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "User Not Found" });
+    }
+
+    let isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (isPasswordCorrect) {
+      let token = crypto.randomBytes(20).toString("hex");
+
+      user.token = token;
+      await user.save();
+      return res.status(httpStatus.OK).json({ token: token });
+    } else {
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json({ message: "Invalid Username or password" });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: `Something went wrong ${e}` });
+  }
+};
 
 const register = async (req, res) => {
   const { name, username, password } = req.body;
@@ -10,45 +46,56 @@ const register = async (req, res) => {
   try {
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(httpStatus.FOUND).json({ message: "User already exists" });
+      return res
+        .status(httpStatus.FOUND)
+        .json({ message: "User already exists" });
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       name: name,
       username: username,
       password: hashedPassword,
     });
+
     await newUser.save();
-    res.status(httpStatus.CREATED).json({ message: "User registered successfully" });
-  } catch (err) {
-    res.json({ message: `Something went wrong ${err}` });
+
+    res.status(httpStatus.CREATED).json({ message: "User Registered" });
+  } catch (e) {
+    res.json({ message: `Something went wrong ${e}` });
   }
 };
 
+const getUserHistory = async (req, res) => {
+  const { token } = req.query;
 
-const login = async (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        return res.status(400).json({ message: "Please Provide" });
-    }
-    try {
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" });
-        }
-        let isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if (isPasswordCorrect) {
-            let token = crypto.randomBytes(20).toString("hex");
-            user.token = token;
-            await user.save();
-            return res.status(httpStatus.OK).json({ token: token });
-        } else {
-          return res.status(httpStatus.UNAUTHORIZED).json({ message: "Invalid Username or Password" });
-        }
-    } catch (err) {
-        return res.status(500).json({ message: `Something went wrong ${err}` });
-    }
+  try {
+    const user = await User.findOne({ token: token });
+    const meetings = await Meeting.find({ user_id: user.username });
+    res.json(meetings);
+  } catch (e) {
+    res.json({ message: `Something went wrong ${e}` });
+  }
 };
 
+const addToHistory = async (req, res) => {
+  const { token, meeting_code } = req.body;
 
-export { register, login };
+  try {
+    const user = await User.findOne({ token: token });
+
+    const newMeeting = new Meeting({
+      user_id: user.username,
+      meetingCode: meeting_code,
+    });
+
+    await newMeeting.save();
+
+    res.status(httpStatus.CREATED).json({ message: "Added code to history" });
+  } catch (e) {
+    res.json({ message: `Something went wrong ${e}` });
+  }
+};
+
+export { login, register, getUserHistory, addToHistory };
